52c8fc4d122f76e5e1d935f5baf56c533141900d
priyanshu <priyanshumandloigurukul@gmail.com>
2024-06-19 01:29:11 +0530

    Updated ResumePage.js, style.css, and yarn.lock

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -5,15 +5,14 @@ import React, { useEffect, useState } from "react";
 
 import { AiOutlineDownload } from "react-icons/ai";
 import Button from "react-bootstrap/Button";
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import Particle from "../../Particle";
 import Zoom from "react-reveal/Zoom";
-import pdf from "../../assets/Pm_resume.pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/priyanshu-mandloi/Portfolio-web/main/src/assets/Pm_resume.pdf";
+// Use the Google Drive Viewer link
+const googleDriveViewerLink = "https://drive.google.com/file/d/1pEepRb9ZaNMR9I2PO8UXpBBWly0uukpZ/view?usp=sharing";
 
 function Resume() {
   const [width, setWidth] = useState(1200);
@@ -30,32 +29,35 @@ function Resume() {
           <Container className="home-content">
             <div
               className="d-flex justify-content-center"
-              width="100%"
               style={{ backgroundColor: "#fbd9ad" }}
             >
               <Zoom left cascade>
                 <h1 style={{ color: "rgb(134 61 176)" }}>RESUME</h1>
               </Zoom>
             </div>
-            <div fluid className="certificate-section" id="about">
+            <Container fluid className="certificate-section" id="about">
               <div className="d-flex justify-content-center mt-4">
-                <Button variant="primary" href={pdf} target="_blank">
+                <Button variant="primary" href={googleDriveViewerLink} target="_blank">
                   <AiOutlineDownload />
                   &nbsp;Download Resume
                 </Button>
               </div>
               <div className="resume d-flex justify-content-center">
-                <Document file={resumeLink}>
-                  <Page pageNumber={1} scale={width > 786 ? 1.6 : 0.4} />
-                </Document>
+                {/* Embed the Google Drive Viewer */}
+                <iframe
+                  title="resume"
+                  src={`https://drive.google.com/file/d/1pEepRb9ZaNMR9I2PO8UXpBBWly0uukpZ/preview`}
+                  width="100%"
+                  height="800px"
+                ></iframe>
               </div>
               <div className="d-flex justify-content-center">
-                <Button variant="primary" href={pdf} target="_blank">
+                <Button variant="primary" href={googleDriveViewerLink} target="_blank">
                   <AiOutlineDownload />
                   &nbsp;Download Resume
                 </Button>
               </div>
-            </div>
+            </Container>
           </Container>
         </Container>
       </section>
